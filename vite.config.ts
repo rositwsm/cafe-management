9045a57af51a660ae64fa9e416326b62b59f7052
rositwsm <rositaeslami1382@gmail.com>
2026-09-13 22:53:13 +0330

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/cafe-management/' : '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
